0eaac853b993464cc64c68eb23cc2e9ba0b702e7
lolforreal <166087670+lolforreal@users.noreply.github.com>
2024-05-16 15:00:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,96 +1,44 @@
-const gameContainer = document.getElementById("game");
-let card1 = null;
-let card2 = null;
-let cardsFlipped = 0;
-let noClicking = false;
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function shuffle(array) {
-  let counter = array.length;
+function search(str) {
+  let results = [];
 
-  // While there are elements in the array
-  while (counter > 0) {
-    // Pick a random index
-    let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
-    counter--;
-
-    // And swap the last element with it
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-
-  return array;
-}
-
-let shuffledColors = shuffle(COLORS);
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add(color);
-    newDiv.addEventListener("click", handleCardClick);
-    gameContainer.append(newDiv);
-  }
-}
-
-function handleCardClick(e) {
-  if (noClicking) return;
-  if (e.target.classList.contains("flipped")) return;
-
-  let currentCard = e.target;
-  currentCard.style.backgroundColor = currentCard.classList[0];
-
-  if (!card1 || !card2) {
-    currentCard.classList.add("flipped");
-    card1 = card1 || currentCard;
-    card2 = currentCard === card1 ? null : currentCard;
-  }
-
-  if (card1 && card2) {
-    noClicking = true;
-    // debugger
-    let gif1 = card1.className;
-    let gif2 = card2.className;
-
-    if (gif1 === gif2) {
-      cardsFlipped += 2;
-      card1.removeEventListener("click", handleCardClick);
-      card2.removeEventListener("click", handleCardClick);
-      card1 = null;
-      card2 = null;
-      noClicking = false;
-    } else {
-      setTimeout(function() {
-        card1.style.backgroundColor = "";
-        card2.style.backgroundColor = "";
-        card1.classList.remove("flipped");
-        card2.classList.remove("flipped");
-        card1 = null;
-        card2 = null;
-        noClicking = false;
-      }, 1000);
+  fruit.forEach(fruit => {
+    if (fruit.toLowerCase().includes(str.toLowerCase())) {
+      results.push(fruit);
     }
-  }
+  });
 
-  if (cardsFlipped === COLORS.length) alert("game over!");
+  return results;
 }
 
-createDivsForColors(shuffledColors);
+function searchHandler(e) {
+  const inputValue = e.target.value.trim();
+  const results = search(inputValue);
+  showSuggestions(results, inputValue);
+}
+
+function showSuggestions(results, inputVal) {
+  suggestions.innerHTML = '';
+
+  results.forEach(result => {
+    const li = document.createElement('li');
+    li.textContent = result;
+    suggestions.appendChild(li);
+  });
+
+  suggestions.classList.toggle('has-suggestions', results.length > 0);
+}
+
+function useSuggestion(e) {
+  if (e.target.tagName === 'LI') {
+    input.value = e.target.textContent;
+    suggestions.innerHTML = '';
+  }
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
